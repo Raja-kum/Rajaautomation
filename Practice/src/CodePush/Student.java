7b708a14d7a148fe913b9ff8d501348c3997f059
hp <hp@DESKTOP-791LALF>
2023-09-09 13:56:40 +0530

    Commit

---
+++ Practice/src/CodePush/Student.java
@@ -0,0 +1,35 @@
+package CodePush;
+
+public class Student
+{
+	int name,age; 
+	
+
+	public Student(){
+		
+	System.out.println("Raja");	
+	}
+	public Student(int name,int age)
+	{
+		System.out.println("Two parameter");
+	}
+	public static void main (String args[])
+	{
+	 Student s=new Student();
+	}
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	{
+}
+
+	}
+
+
